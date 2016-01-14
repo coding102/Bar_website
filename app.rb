b1c782e4bf7f6397fb0319a58ca7db8e4dddc2fa
Marck <mercercountynj@gmail.com>
2016-01-14 10:29:41 -0500

    commenting

--- app.rb
+++ app.rb
@@ -3,29 +3,33 @@ require 'sinatra'
 require 'sendgrid-ruby'
 
 
-
+# home direction
 get '/' do
     erb :home
 end
 
+#menu direction
 get '/menu' do
     erb :menu
 end
 
+#events direction
 get '/events' do
     erb :events
 end
 
+#bar direction
 get '/bar' do
     erb :bar
 end
 
+#contact direction
 get '/contact' do
     erb :contact 
 end
 
 
-
+#posting to email form in contact.erb 
 post '/email-request' do
 
     
@@ -35,11 +39,12 @@ post '/email-request' do
 	   @phone = params['phone']
 	   @description = params['description']
     
-    
+    #sendgrid client validation
     client = SendGrid::Client.new do |c|
         c.api_key = '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'
     end
     
+    #routing html form to post request
     mail = SendGrid::Mail.new do |m|
         m.to = 'mercercountynj@gmail.com'
         m.from = @email
@@ -53,7 +58,6 @@ post '/email-request' do
     puts res.code
     puts res.body
     
-
     erb :home
     
 end
